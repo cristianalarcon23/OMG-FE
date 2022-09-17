47ab4d7ca895bfb271632f6abfda179f25a63c55
calarcon <calarcon@leadtech.com>
2022-09-17 18:36:46 +0200

    add item view css done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Signup from './views/auth/Signup';
 import Login from './views/auth/Login';
 import PrivateView from './views/PrivateView';
 import IsPrivate from './components/IsPrivate';
-import AddItem from './views/items/AddItem';
+import AddItem2 from './views/items/AddItem2';
 import Home from './views/Home';
 import Items from './views/items/Items';
 import Search from './views/Search';
@@ -26,7 +26,7 @@ function App() {
         <Route path="/search" element={<Search />} />
         <Route path="/items" element={<IsPrivate><Items /></IsPrivate>} />
         <Route path="/items/:id" element={<IsPrivate><ItemDetail /></IsPrivate>} />
-        <Route path="/items/add" element={<IsPrivate><AddItem /></IsPrivate>} />
+        <Route path="/items/add" element={<IsPrivate><AddItem2 /></IsPrivate>} />
         <Route path="/items/edit/:id" element={<IsPrivate><EditItem /></IsPrivate>} />
         <Route path="/transactions" element={<IsPrivate><Transactions /></IsPrivate>} />
         <Route path="/transactions/receive" element={<IsPrivate><Receive /></IsPrivate>} />
