55a28166ab99364e5478d4bdd9b7a9cc01da2862
calarcon <calarcon@leadtech.com>
2022-09-17 15:54:25 +0200

    css for login and register done

--- src/views/auth/Signup.jsx
+++ src/views/auth/Signup.jsx
@@ -3,66 +3,195 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 export default function Signup() {
-  const [user, setUser] = useState({
-    email: '',
-    fullName: '',
-    username: '',
-    idNumber: '',
-    telephone: ''
-  })
-  const [password, setPassword] = useState('');
-  const [passwordControl, setPasswordControl] = useState('');
-  const [errorMessage, setErrorMessage] = useState(undefined);
-  const navigate = useNavigate();
-
-  const handleChange = (e) => {
-    setUser(prev => {
-      return {
-        ...prev,
-        [e.target.name]: e.target.value
-      }
+    const [user, setUser] = useState({
+      email: '',
+      fullName: '',
+      username: '',
+      idNumber: '',
+      telephone: ''
     })
-  }
-
-  useEffect(() => {
-    if (password !== passwordControl) {
-      setErrorMessage("Passwords don't match")
-    } else {
-      setErrorMessage(undefined)
+    const [password, setPassword] = useState('');
+    const [passwordControl, setPasswordControl] = useState('');
+    const [errorMessage, setErrorMessage] = useState(undefined);
+    const navigate = useNavigate();
+  
+    const handleChange = (e) => {
+      setUser(prev => {
+        return {
+          ...prev,
+          [e.target.name]: e.target.value
+        }
+      })
     }
-    // eslint-disable-next-line
-  }, [passwordControl])
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await axios.post(`${process.env.REACT_APP_API_URL}/auth/signup`, { username: user.username, email: user.email, password, fullName: user.fullName, idNumber: user.idNumber, telephone: user.telephone });
-      navigate('/login');
-    } catch (error) {
-      setErrorMessage(error.response.data.error)
+  
+    useEffect(() => {
+      if (password !== passwordControl) {
+        setErrorMessage("Passwords don't match")
+      } else {
+        setErrorMessage(undefined)
+      }
+      // eslint-disable-next-line
+    }, [passwordControl])
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+        await axios.post(`${process.env.REACT_APP_API_URL}/auth/signup`, { username: user.username, email: user.email, password, fullName: user.fullName, idNumber: user.idNumber, telephone: user.telephone });
+        navigate('/login');
+      } catch (error) {
+        setErrorMessage(error.response.data.error)
+      }
     }
-  }
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>Email</label>
-        <input required type="email" name="email" value={user.email} onChange={handleChange} />
-        <label>Full Name</label>
-        <input required type="text" name="fullName" value={user.fullName} onChange={handleChange} />
-        <label>Username</label>
-        <input required type="text" name="username" value={user.username} onChange={handleChange} />
-        <label>Password</label>
-        <input required type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value) } />
-        <label>Repeat the password</label>
-        <input required type="password" name="passwordControl" value={passwordControl} onChange={(e) => setPasswordControl(e.target.value)} />
-        <label>Id number</label>
-        <input required type="text" name="idNumber" value={user.idNumber} onChange={handleChange} />
-        <label>Telephone</label>
-        <input required type="text" name="telephone" value={user.telephone} onChange={handleChange} />
-        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-        <button type="submit">Register</button>
-      </form>
-    </div>
-  )
-}
+    return (
+        <>
+  
+          <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+              <img
+                className="mx-auto h-12 w-auto"
+                src="https://pic.onlinewebfonts.com/svg/img_542832.png"
+                alt="Your Company"
+              />
+              <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Register your account</h2>
+            </div>
+    
+            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+              <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                <form className="space-y-6" action="#" onSubmit={handleSubmit}>
+                  <div>
+                    <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                      Email address
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        autoComplete="email"
+                        required
+                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        value={user.email}
+                        onChange={handleChange}
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <label htmlFor="fullName" className="block text-sm font-medium text-gray-700">
+                      Full name
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        id="fullName"
+                        name="fullName"
+                        type="text"
+                        required
+                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        value={user.fullName}
+                        onChange={handleChange}
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <label htmlFor="username" className="block text-sm font-medium text-gray-700">
+                      Username
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        id="username"
+                        name="username"
+                        type="text"
+                        required
+                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        value={user.username}
+                        onChange={handleChange}
+                      />
+                    </div>
+                  </div>
+    
+                  <div>
+                    <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                      Password
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        required
+                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value) }
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                      Repeat the password
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        id="passwordControl"
+                        name="passwordControl"
+                        type="password"
+                        required
+                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        value={passwordControl}
+                        onChange={(e) => setPasswordControl(e.target.value) }
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <label htmlFor="idNumber" className="block text-sm font-medium text-gray-700">
+                      ID Number
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        id="idNumber"
+                        name="idNumber"
+                        type="text"
+                        required
+                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        value={user.idNumber}
+                        onChange={handleChange}
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <label htmlFor="username" className="block text-sm font-medium text-gray-700">
+                      Telephone
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        id="telephone"
+                        name="telephone"
+                        type="text"
+                        required
+                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        value={user.telephone}
+                        onChange={handleChange}
+                      />
+                    </div>
+                  </div>
+    
+                  <div>
+                    <button
+                      type="submit"
+                      className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    >
+                      Register
+                    </button>
+                  </div>
+                  {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+                </form>
+              </div>
+            </div>
+          </div>
+        </>
+      )
+    }
