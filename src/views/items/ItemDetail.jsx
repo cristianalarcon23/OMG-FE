b0611d5b90bfc917b115db96a44755b0a71a009e
calarcon <calarcon@leadtech.com>
2022-09-20 20:25:01 +0200

    item views and item details done

--- src/views/items/ItemDetail.jsx
+++ src/views/items/ItemDetail.jsx
@@ -83,28 +83,97 @@ const handleDelete = async () => {
   }
 
   return (
-    <div>
-      {itemDetail && 
-        <div>
-            <h1>Model: {itemDetail.name}</h1>
-            <p>Type: {itemDetail.type}</p>
-            <p>Brand: {itemDetail.brand}</p>
-            <p>Serial Number: {itemDetail.serialNumber}</p>
-            <p>Item picture: </p>
-            {itemDetail.imageUrls.map(item => {
-         return <div key={item._id}><img src={item} alt="" srcset="" /></div>
-        })}
-            <p>Bought new?: {itemDetail.newItem}</p>
-            {itemDetail.transactionToken && <p>Transfer token: {itemDetail.transactionToken}</p>}
-            {itemDetail.previousOwner && <p>Previous owner: {itemDetail.previousOwner.username}</p> }
-            {alert && <p style={{ color: 'red' }}>This item has an stolen alert</p>}
-            <button onClick={() => navigate(`/items/edit/${id}`)}>Edit item</button>
-            <button onClick={handleDelete}>Delete item</button>
-            {!alert ? <button onClick={handleAlert}>Mark item as lost</button> : <button onClick={deleteAlert}>Unmark item as lost</button>}
-            {!itemDetail.transactionToken ? <button onClick={handleToken}>Generate transfer token</button> : <button onClick={deleteToken}>Delete transfer token</button>}
-            {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+    <>
+     {itemDetail && <div className="px-4 sm:px-6 lg:px-8">
+     <div className="mt-20 flex flex-col">
+       <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+         <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+           <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+           {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+             <table className="min-w-full divide-y divide-gray-300">
+               <thead className="bg-gray-50">
+                 <tr className="divide-x divide-gray-200">
+                   <th scope="col" className="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                     Model
+                   </th>
+                   <th scope="col" className="px-4 py-3.5 text-left text-sm font-semibold text-gray-900">
+                     Brand
+                   </th>
+                   <th scope="col" className="px-4 py-3.5 text-left text-sm font-semibold text-gray-900">
+                     Type
+                   </th>
+                   <th scope="col" className="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pr-6">
+                     Serial Number
+                   </th>
+                   <th scope="col" className="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pr-6">
+                     Stolen alert
+                   </th>
+                   <th scope="col" className="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pr-6">
+                     Transfer token
+                   </th>
+                 </tr>
+               </thead>
+               <tbody className="divide-y divide-gray-200 bg-white">
+               {itemDetail &&<tr className="divide-x divide-gray-200">
+                      <td className="whitespace-nowrap py-4 pl-4 pr-4 text-sm font-medium text-gray-900 sm:pl-6">
+                       {itemDetail.name}
+                     </td>
+                     <td className="whitespace-nowrap p-4 text-sm text-gray-500">{itemDetail.brand}</td>
+                     <td className="whitespace-nowrap p-4 text-sm text-gray-500">{itemDetail.type}</td>
+                     <td className="whitespace-nowrap py-4 pl-4 pr-4 text-sm text-gray-500 sm:pr-6">{itemDetail.serialNumber}</td>
+                     <td className="whitespace-nowrap p-4 text-sm text-gray-500">{alert ? <p style={{ color: 'red' }}>Stolen alert</p> : <p>No alerts set</p> }</td>
+                     <td className="whitespace-nowrap py-4 pl-4 pr-4 text-sm text-gray-500 sm:pr-6">{itemDetail.transactionToken ? <p>{itemDetail.transactionToken}</p> : <p>No token</p> }</td>
+                   </tr>}
+               </tbody>
+             </table>
+           </div>
+           <div className="sm:flex sm:items-center">
+       <div className="sm:flex-auto">
+         <p className="mt-20 text-sm text-gray-700">  
+         </p>
+       </div>
+       <div className="mt-20 sm:mt-0 sm:ml-16 sm:flex-none">
+         <button
+           type="button"
+           className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+           onClick={() => navigate(`/items/edit/${id}`)}
+         >
+           Edit item
+         </button>
+       </div>
+       <div className="mt-20 sm:mt-0 sm:ml-16 sm:flex-none">
+         <button
+           type="button"
+           className="inline-flex items-center justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:w-auto"
+           onClick={handleDelete}
+         >
+           Delete item
+         </button>
+       </div>
+       <div className="mt-20 sm:mt-0 sm:ml-16 sm:flex-none">
+         {!alert ? <button className="inline-flex items-center justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:w-auto" onClick={handleAlert}>Mark item as lost</button> : <button className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto" onClick={deleteAlert}>Unmark item as lost</button>}
+       </div>
+       <div className="mt-20 sm:mt-0 sm:ml-16 sm:flex-none">
+       {!itemDetail.transactionToken ? <button className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto" onClick={handleToken}>Generate token</button> : <button className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto" onClick={deleteToken}>Delete token</button>}
+       </div>
+     </div>
+         </div>
+       </div>
+     </div>
+        <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        {itemDetail.imageUrls && itemDetail.imageUrls.map((item) => (
+              <>
+              <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+                <img
+                  src={item}
+                  alt={item.name}
+                  className="h-full w-full object-cover object-center group-hover:opacity-75"
+                />
+              </div>
+              </>
+          ))}
         </div>
-      }
-    </div>
+   </div>}
+   </>
   )
 }
--- src/views/items/Items.jsx
+++ src/views/items/Items.jsx
@@ -21,13 +21,25 @@ export default function Items() {
     },[])
     return (
         <>
-        <h1>Items</h1>
-      <li><Link to="/items/add">Add a new item</Link></li>
-      <div>
-        {items && items.map(item => {
-         return <div key={item._id}><p><Link to={`${item._id}`}>{item.name}</Link></p></div>
-        })}
+      <div className="bg-white">
+      <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="sr-only">My items</h2>
+        <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {items &&items.map((item) => (
+            <Link key={item._id} to={`${item._id}`} className="group">
+              <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+                <img
+                  src={item.imageUrls[0]}
+                  alt={item.name}
+                  className="h-full w-full object-cover object-center group-hover:opacity-75"
+                />
+              </div>
+              <h3 className="mt-4 text-sm text-gray-700">{item.name}</h3>
+            </Link>
+          ))}
+        </div>
       </div>
+    </div>
       </>
     )
   }
