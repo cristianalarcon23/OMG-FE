914824a098a03194ce74d6939087f091abce2858
calarcon <calarcon@leadtech.com>
2022-09-24 13:26:58 +0200

    minor toaste fixes

--- src/views/auth/Signup.jsx
+++ src/views/auth/Signup.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { Toast } from 'react-hot-toast';
+import toast  from 'react-hot-toast';
 
 export default function Signup() {
     const [user, setUser] = useState({
